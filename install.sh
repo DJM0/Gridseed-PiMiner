e3f743f2d705ead5d6f3f3de8b21bd407cb93d62
David Maitland <david.maitland@sipcentric.com>
2014-06-11 19:08:06 +0100

    Added install.sh script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Gridseed single line installer aimed for Raspbian on Raspberry Pi 
+# install.sh
+# DavidMaitland.me & josh.farrant.me 2014
+
+# This script will do the following:
+# 1. Install the requirements for building dtbartle version of cgminer
+# 2. Get a copy of cgminer
+# 3. Configure, make and make install
+# 4. Setup config.json for mining pool
+# 5. Add onboot script for automatically running miner
+# 6. Add script for ease of starting miner
+
+echo "Hello World!"
